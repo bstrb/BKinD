82eae5b2cdffe06b15964af23226611259b99325
Buster Blomberg <buster.blomberg@gmail.com>
2024-07-08 07:03:15 +0200

    modified:   BKinD_3.0/src/gui/get_input.py

--- BKinD_3.0/src/gui/get_input.py
+++ BKinD_3.0/src/gui/get_input.py
@@ -4,7 +4,7 @@
 from tkinter import messagebox
 
 # def no_unnecessary_zeros(number):
-·#     return float(f"{number:.10g}")
+#     return float(f"{number:.10g}")
 
 def get_input(self, crystal_name):
     try:

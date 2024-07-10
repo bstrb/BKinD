7307af647d8c488ba0b6745248655ebb8ee26cf6
Buster Blomberg <buster.blomberg@gmail.com>
2024-07-10 07:14:46 +0200

    new file:   BKinD_3.0/src/util/test/comapre_mol_2.py 	modified:   BKinD_3.0/src/util/test/compare_molecules.py 	new file:   BKinD_3.0/src/util/test/matrixplay.py

--- BKinD_3.0/src/util/test/compare_molecules.py
+++ BKinD_3.0/src/util/test/compare_molecules.py
@@ -1,4 +1,5 @@
 # %%
+
 import os
 from read_coordinates import read_coordinates
 
@@ -39,33 +40,38 @@ def compare_matrices(matrix1, matrix2, tolerance=0.1):
     best_permutation, total_sum_diff = find_best_match_columnwise(matrix1, matrix2)
     permuted_matrix2 = matrix2[:, best_permutation]
     
-    print(f"Total sum of differences: {total_sum_diff:.4f}")
-    print(f"Best permutation: {best_permutation}")
-    print("Best matches (column-wise):")
+    # print(f"Total sum of differences: {total_sum_diff:.4f}")
+    # print(f"Best permutation: {best_permutation}")
+    # print("Best matches (column-wise):")
     for i, j in enumerate(best_permutation):
         col_from_A = matrix1[:, i]
         col_from_B = permuted_matrix2[:, i]
         diff_matrix = periodic_difference(col_from_A, col_from_B)
-        print(f"Column from A: {col_from_A}, Column from B: {col_from_B}, Difference: {diff_matrix}, Sum of Differences: {np.sum(diff_matrix):.4f}")
+        print(f"Column from A: {col_from_A},")
+        print(f"Column from B: {col_from_B},")
+        print(f"Difference: {diff_matrix},")
+        print(f"Sum of Differences: {np.sum(diff_matrix):.4f}")
     
     return total_sum_diff < tolerance
 
-path_res_100 = '/Users/xiaodong/Desktop/bkind_LTA_to_90.0_completeness/filtered_99.43'
-# Example usage for solve_filtered.ins
-file_name_res_100 = os.path.join(path_res_100, 'filtered_99.43_a.res')
-coords_res_100 = read_coordinates(file_name_res_100, 'PLAN', 'HKLF 4')
-print("Coordinates from filtered_100.0_a.res:")
-print(coords_res_100)
+file_name_res_orig = '/Users/xiaodong/Desktop/bkind_XRAY_feacacmerge_to_89.0_completeness/filtered_91.87/filtered_91.87_a.res'
+file_name_res = '/Users/xiaodong/Desktop/bkind_XRAY_feacacmerge_to_89.0_completeness/filtered_89.0/solve_filtered/solve_filtered_a.res'
 
-path_res = '/Users/xiaodong/Desktop/bkind_LTA_to_90.0_completeness/filtered_90.0/solve_filtered'
-file_name_res = os.path.join(path_res, 'solve_filtered_a.res')
+
+file_name_res_orig = '/Users/xiaodong/Desktop/bkind_XRAY_feacacmerge_to_89.0_completeness/filtered_91.87/filtered_91.87_a.res'
+file_name_res = '/Users/xiaodong/Desktop/bkind_XRAY_feacacmerge_to_89.0_completeness/filtered_89.0/solve_filtered/solve_filtered_a.res'
+
+
+coords_res_orig = read_coordinates(file_name_res_orig, 'PLAN', 'HKLF 4')
+# print("Coordinates from filtered_orig_a.res:")
+# print(coords_res_orig)
 coords_res = read_coordinates(file_name_res, 'PLAN', 'HKLF 4')
-print("Coordinates from solve_filtered_a.res:")
-print(coords_res)
+# print("Coordinates from solve_filtered_a.res:")
+# print(coords_res)
 
-# print(len(coords_res_100))
+# print(len(coords_res_orig))
 # print(len(coords_res))
-compare_ins_res = compare_matrices(coords_res_100, coords_res)
+compare_ins_res = compare_matrices(coords_res_orig, coords_res)
 
 print(f"Are the molecules the same? {compare_ins_res}")
 # %%

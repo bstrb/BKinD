82a1251567567bb0600234abaae423fb961fd1ef
Buster Blomberg <buster.blomberg@gmail.com>
2024-07-12 14:41:24 +0200

    modified:   BKinD_3.0/default.profraw 	modified:   BKinD_3.0/src/util/test/compare_matrices.py 	new file:   BKinD_3.0/src/util/test/find_best_permutation.py 	new file:   BKinD_3.0/src/util/test/generate_column_permutations.py 	modified:   BKinD_3.0/src/util/test/generate_complementary_matrices.py 	new file:   BKinD_3.0/src/util/test/generate_row_permutations.py

--- BKinD_3.0/src/util/test/compare_matrices.py
+++ BKinD_3.0/src/util/test/compare_matrices.py
@@ -1,47 +1,72 @@
-# compare_matrices.py
-
 # %%
 
 import numpy as np
-
-from generate_permutations import generate_permutations
+import concurrent.futures
+from generate_column_permutations import generate_column_permutations
+from generate_row_permutations import generate_row_permutations
 from generate_complementary_matrices import generate_complementary_matrices
-from subtract_and_sum_matrix_groups import subtract_and_sum_matrix_groups
 from read_coordinates import read_coordinates
 
+def compare_matrices(permuted_matrix, complementary_matrix):
+    return np.sum(np.abs(permuted_matrix - complementary_matrix))
 
-file_name_res_orig = '/mnt/c/Users/bubl3932/Desktop/bkind_XRAY_FEACAC_to_89.0_completeness/filtered_91.87/filtered_91.87_a.res'
-file_name_res = '/mnt/c/Users/bubl3932/Desktop/bkind_XRAY_FEACAC_to_89.0_completeness/filtered_89.0/solve_filtered/solve_filtered_a.res'
+if __name__ == "__main__":
+    # File paths
+    file_name_res_orig = '/Users/xiaodong/Desktop/bkind_FEACAC13_to_79.0_completeness/bkind_a.res'
+    file_name_res = '/Users/xiaodong/Desktop/bkind_FEACAC13_to_79.0_completeness/filtered_79.0/removed_data_79.0/removed_data_79.0_a.res'
 
-file_name_res_orig = '/mnt/c/Users/bubl3932/Desktop/bkind_LTA_to_96.0_completeness/bkind_a.res'
-file_name_res = '/mnt/c/Users/bubl3932/Desktop/bkind_LTA_to_96.0_completeness/filtered_96.0/solve_filtered_96.0/solve_filtered_96.5_a.res'
+    coords_res_orig = read_coordinates(file_name_res_orig, 'PLAN', 'HKLF 4')
+    coords_res = read_coordinates(file_name_res, 'PLAN', 'HKLF 4')
 
-coords_res_orig = read_coordinates(file_name_res_orig, 'PLAN', 'HKLF 4')
-coords_res = read_coordinates(file_name_res, 'PLAN', 'HKLF 4')
+    matrix1 = coords_res_orig
+    matrix2 = coords_res
 
-matrix1 = coords_res_orig
-matrix2 = coords_res
+    # Generate column permutations
+    column_permutations = generate_column_permutations(matrix1)
 
-# Generate all permutations of matrix1 swapping rows and columns
-all_permuted_matrices = generate_permutations(matrix1)
+    # Generate complementary matrices
+    complementary_matrices = generate_complementary_matrices(matrix2)
 
-# Generate all complementary matrices to matrix 2
-complementary_matrices = generate_complementary_matrices(matrix2)
+    # Parallel processing of column comparisons
+    column_results = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = [executor.submit(compare_matrices, perm, comp) for perm in column_permutations for comp in complementary_matrices]
+        for future in concurrent.futures.as_completed(futures):
+            column_results.append(future.result())
 
-# Perform subtraction and summation of absolute differences
-results, combinations = subtract_and_sum_matrix_groups(all_permuted_matrices, complementary_matrices)
+    # Find the best column permutation and complementary matrix combination
+    min_column_sum = min(column_results)
+    min_column_index = column_results.index(min_column_sum)
+    best_col_perm_index = min_column_index // len(complementary_matrices)
+    best_comp_matrix_index = min_column_index % len(complementary_matrices)
 
-# Find the minimum sum and corresponding matrix combination
-min_sum = min(results)
-min_index = results.index(min_sum)
-min_combination = combinations[min_index]
+    best_col_permuted_matrix = column_permutations[best_col_perm_index]
+    best_complementary_matrix = complementary_matrices[best_comp_matrix_index]
 
-# Display the minimum sum and the matrices that gave the minimum sum
-print(f"Minimum sum of all resulting matrices: {min_sum}")
-print(f"Matrix A index: {min_combination[0]}")
-print(f"Matrix B index: {min_combination[1]}")
-print(f"Resulting matrix:\n{min_combination[2]}")
-print(f"Matrix A:\n{all_permuted_matrices[min_combination[0]]}")
-print(f"Matrix B:\n{complementary_matrices[min_combination[1]]}")
+    # Generate row permutations for the best column-matched matrices
+    # row_permutations = generate_row_permutations(best_col_permuted_matrix)
+
+    # Compare row permutations with the best complementary matrix
+    # row_results = []
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    #     futures = [executor.submit(compare_matrices, perm, best_complementary_matrix) for perm in row_permutations]
+    #     for future in concurrent.futures.as_completed(futures):
+    #         row_results.append(future.result())
+
+    # Find the best row permutation
+    # min_row_sum = min(row_results)
+    # min_row_index = row_results.index(min_row_sum)
+    # best_row_perm_index = min_row_index
+
+    # Display the minimum sum and the matrices that gave the minimum sum
+    print(f"Minimum sum of column permutations: {min_column_sum}")
+    # print(f"Minimum sum of row permutations: {min_row_sum}")
+    print(f"Best column-permuted matrix index: {best_col_perm_index}")
+    print(f"Best complementary matrix index: {best_comp_matrix_index}")
+    # print(f"Best row-permuted matrix index: {best_row_perm_index}")
+
+    print(f"Best column-permuted matrix:\n{best_col_permuted_matrix}")
+    print(f"Best complementary matrix:\n{best_complementary_matrix}")
+    # print(f"Best row-permuted matrix:\n{row_permutations[best_row_perm_index]}")
 
 # %%

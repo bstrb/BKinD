1d33fa29d9923dbb607a726feb0286340a5ee060
Buster Blomberg <buster.blomberg@gmail.com>
2024-07-12 14:41:24 +0200

    modified:   BKinD_3.0/default.profraw 	modified:   BKinD_3.0/src/util/test/compare_matrices.py 	new file:   BKinD_3.0/src/util/test/find_best_permutation.py 	new file:   BKinD_3.0/src/util/test/generate_column_permutations.py 	modified:   BKinD_3.0/src/util/test/generate_complementary_matrices.py 	new file:   BKinD_3.0/src/util/test/generate_row_permutations.py

--- BKinD_3.0/src/util/test/generate_complementary_matrices.py
+++ BKinD_3.0/src/util/test/generate_complementary_matrices.py
@@ -4,6 +4,15 @@ import numpy as np
 from itertools import product
 
 def generate_complementary_matrices(matrix):
+    """
+    Generate all complementary matrices by flipping columns.
+    
+    Args:
+    matrix (np.ndarray): The input matrix of shape (rows, cols).
+    
+    Returns:
+    List[np.ndarray]: A list of complementary matrices.
+    """
     rows, cols = matrix.shape
     matrices = []
     
@@ -12,7 +21,9 @@ def generate_complementary_matrices(matrix):
         new_matrix = np.copy(matrix)
         for col_index in range(cols):
             if comb[col_index] == 1:
-                new_matrix[:, col_index] = np.abs(1 - new_matrix[:, col_index])
+                # Flip the column values (assuming binary matrix with 0 and 1)
+                new_matrix[:, col_index] = 1 - new_matrix[:, col_index]
         matrices.append(new_matrix)
     
     return matrices
+

1aab78eb151ab02b7a3eee0e09cd6fcb2ac88cc1
Buster Blomberg <buster.blomberg@gmail.com>
2024-11-08 09:22:30 +0100

    modified:   .DS_Store 	modified:   SSED/SSED_IQM_v3/calculate_combined_metric.py

--- SSED/SSED_IQM_v3/calculate_combined_metric.py
+++ SSED/SSED_IQM_v3/calculate_combined_metric.py
@@ -1,7 +1,7 @@
-# Define function for combined metric calculation using product of (1 + metric) ^ (metric weight)
+# Define function for combined metric calculation using sum of metric value multiplied by its weight
 def calculate_combined_metric(index, all_metrics, metric_weights):
-    combined_metric = 1  # Start with 1 for multiplication
+    combined_metric = 0  # Start with 0 for summation
     for metric, weight in metric_weights.items():
         metric_value = all_metrics[metric][index]
-        combined_metric *= (1 + metric_value) ** weight
+        combined_metric += metric_value * weight
     return combined_metric

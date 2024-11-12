72bc4c9b72cdb0e8bdb6393f486334e0a22dede8
Buster Blomberg <buster.blomberg@gmail.com>
2024-11-12 10:48:46 +0100

    modified:   SSED/IQM_PROJECT/SSED_EMP/generate_bash_script.py 	modified:   SSED/IQM_PROJECT/SSED_IQM/generate_bash_script.py 	modified:   SSED/IQM_PROJECT/SSED_IQM/iqm/generate_bash_script.py 	modified:   SSED/IQM_PROJECT/SSED_IQM_v2/generate_bash_script.py 	modified:   SSED/IQM_PROJECT/SSED_IQM_v3/generate_bash_script.py 	modified:   SSED/IQM_PROJECT/SSED_RCIS/generate_bash_script.py 	modified:   SSED/IQM_PROJECT/SSED_RCIS_v2/generate_bash_script.py 	modified:   SSED/deice_h5_files/remove_ice_rings.py

--- SSED/deice_h5_files/remove_ice_rings.py
+++ SSED/deice_h5_files/remove_ice_rings.py
@@ -92,8 +92,8 @@ def remove_ice_rings(input_file, output_file, min_spots=10, inner_radius=100, ou
         print(f"Error processing file {input_file}: {e}")
 
 if __name__ == "__main__":
-    input_file = "/home/buster/UOX123/UOX3_minpeaks_15.h5"  # Example input file path
-    output_file = "/home/buster/UOX123/deiced_UOX3_minpeaks_15.h5"  # Example output file path
+    input_file = "/home/buster/UOX1/UOX1_min_15_peak.h5"  # Example input file path
+    output_file = "/home/buster/UOX1/deiced_UOX1_min_15_peak.h5"  # Example output file path
     min_spots = 5  # Set the threshold for minimum number of spots within the annulus
     inner_radius = 60  # Set the inner radius of the annulus in pixels
     outer_radius = 155  # Set the outer radius of the annulus in pixels
